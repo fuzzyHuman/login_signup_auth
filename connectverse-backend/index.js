3452b55a438fae8d3d53799500b95312480c924f
fuzzyHuman <sharma.sudeep13@gmail.com>
2024-08-22 17:27:04 +0530

    built backend, including middleware for auth , db connection and routes

---
+++ connectverse-backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+require('dotenv').config();
+// Connect to database
+connectDB();
+
+// Middleware for parsing JSON bodies
+app.use(express.json({ extended: false }));
+
+// Define routes
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ connectverse-backend/routes/auth.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Signup Route
+router.post('/signup', async (req, res) => {
+  console.log('Received signup request:', req.body);
+  const { username, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({ username, email, password });
+
+    await user.save();
+
+    const payload = { user: { id: user.id } };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '5h' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Login Route
+router.post('/login', async (req, res) => {
+  console.log('Received login request:', req.body);
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid Credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid Credentials' });
+    }
+
+    const payload = { user: { id: user.id } };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '5h' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
